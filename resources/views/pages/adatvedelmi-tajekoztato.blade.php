38b38227d35595a37938d07bdaac0e7d7e36240a
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-07-10 12:43:39 +0200

    fix: Update contact phone number to reflect new brand Somigumi

--- resources/views/pages/adatvedelmi-tajekoztato.blade.php
+++ resources/views/pages/adatvedelmi-tajekoztato.blade.php
@@ -316,7 +316,7 @@
                                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z">
                                     </path>
                                 </svg>
-                                +36 1 415 2075
+                                +36 30 700 9668
                             </a>
                             <a href="mailto:info@somigumiwebaruhaz.hu"
                                 class="flex items-center bg-white text-brand-blue px-6 py-3 rounded-lg font-semibold hover:bg-gray-200 transition">
